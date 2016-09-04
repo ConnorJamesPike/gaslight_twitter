65ebb0ed374cdbcf6e16334dc4b6de41c193e0ac
Connor J. Pike <connorjamespike@gmail.com>
2016-09-04 17:28:03 -0400

    User Model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+	before_save { self.email = email.downcase }
+	validates :username, presence: true, length: { maximum: 15}, uniqueness: { case_sensitive: true}
+	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 }, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+	PASSWORD_REGEX = /\A(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$\z/
+	has_secure_password
+	validates :password, presence: true, format: { with: PASSWORD_REGEX }, length: { minimum: 8, maximum: 16 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,102 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+	@user = User.new(username: "MadeUpUser", email: "fake@foo.com", password: "Bl@hblah", password_confirmation: "Bl@hblah")
+  end
+  
+  test "user validity" do
+	assert @user.valid?
+  end
+  
+  test "username required" do
+	@user.username = " "
+	assert_not @user.valid?
+  end
+  
+  test "email required" do
+	@user.email = " "
+	assert_not @user.valid?
+  end
+  
+  test "password required" do
+	@user.password = @user.password_confirmation = " " * 5
+	assert_not @user.valid?
+  end
+  
+  test "username max length" do
+	@user.username = "a" * 16
+	assert_not @user.valid?
+  end
+  
+  test "email max length" do
+	@user.email = "a" * 255 + "@foo.com"
+	assert_not @user.valid?
+  end
+  
+  test "password min length" do
+	@user.password = @user.password_confirmation = "a" * 7
+	assert_not @user.valid?
+  end
+  
+  test "password max length" do
+	@user.password = @user.password_confirmation = "a" * 17
+	assert_not @user.valid?
+  end
+  
+  test "email valid format" do
+	valid_emails = %w[fake@fake.com fak3.notreal@fake.Com f_ak-3.@fake.foo.com fake+3@gmail.com]
+	valid_emails.each do |valid_email|
+		@user.email = valid_email
+		assert @user.valid?, "#{valid_email.inspect} should be valid"
+	end
+  end
+  
+  test "email invalid format" do
+	invalid_emails = %w[fake.com fake@fake,com n@me@fake.com fake@fake.com+org]
+	invalid_emails.each do |invalid_email|
+		@user.email = invalid_email
+		assert_not @user.valid?
+	end
+  end
+  
+  test "password valid format" do
+	valid_passwords = %w[p@ssWord passW0rd]
+	valid_passwords.each do |valid_password|
+		@user.password = @user.password_confirmation = valid_password
+		assert @user.valid?
+	end
+  end
+  
+  test "password invalid format" do
+	invalid_passwords = %w[password PASSWORD PassWord p@ssword PASSW0RD]
+	invalid_passwords.each do |invalid_password|
+		@user.password = @user.password_confirmation = invalid_password
+		assert_not @user.valid?
+	end
+  end
+  
+  test "unique user" do
+	dup_user = @user.dup
+	@user.save
+	assert_not dup_user.valid?
+  end
+  
+  test "save emails as lowercase" do
+	camel_email = "fake@Fake.Com"
+	@user.email = camel_email
+	@user.save
+	assert_equal camel_email.downcase, @user.reload.email
+  end
+  
+  test "correct password authenticates" do
+	@user.save
+	assert !!@user.authenticate("Bl@hblah")
+  end
+  
+  test "incorrect password does not authenticate" do
+	@user.save
+	assert_not !!@user.authenticate("blahblah")
+  end
 end
